75cc5340410cfcfc640a9bda2217d9f72eaa4afb
Jefferson de Andrade Santos <jersobh@gmail.com>
2016-11-30 15:05:45 -0200

    Função geraItau()

--- www/api/app/src/Controller/RemessaController.php
+++ www/api/app/src/Controller/RemessaController.php
@@ -4,67 +4,78 @@ namespace App\Controller;
 
 use \CnabPHP\Remessa;
 use \CnabPHP\Retorno;
+use Slim\Container;
 
 final class RemessaController
 {
+    protected $mongodb;
+
+    function __construct(Container $c)
+    {
+        $this->mongodb = $c->get('mongodb');
+    }
 
     function geraItau($dados)
     {
-        $arquivo = new Remessa(341, 'Cnab400',
+        $registros         = $this->mongodb->remessas->count();
+        $numero_sequencial = $registros + 1;
+        $arquivo           = new Remessa(341, 'cnab400',
             array(
-            'nome_empresa' => "Empresa ABC", // seu nome de empresa
+            'nome_empresa' => $dados->razao_social, // seu nome de empresa
             'tipo_inscricao' => 2, // 1 para cpf, 2 cnpj 
-            'numero_inscricao' => '12345678901234', // seu cpf ou cnpj completo
-            'agencia' => '1234', // agencia sem o digito verificador 
-            'agencia_dv' => 1, // somente o digito verificador da agencia 
-            'conta' => '12345', // número da conta
-            'conta_dac' => 1, // digito da conta
-            'codigo_beneficiario' => '123456', // codigo fornecido pelo banco
-            'numero_sequencial_arquivo' => $dados->nosso_numero, // sequencial do arquivo um numero novo para cada arquivo gerado
+            'numero_inscricao' => $dados->cnpj, // seu cpf ou cnpj completo
+            'agencia' => $dados->agencia, // agencia sem o digito verificador
+            'agencia_dv' => $dados->agencia_dv, // somente o digito verificador da agencia
+            'conta' => $dados->conta, // número da conta
+            'conta_dv' => $dados->conta_dv, // digito da conta
+            'codigo_beneficiario' => $dados->codigo_beneficiario, // codigo fornecido pelo banco
+            'numero_sequencial_arquivo' => $numero_sequencial, // sequencial do arquivo um numero novo para cada arquivo gerado
         ));
-        $lote    = $arquivo->addLote(array('tipo_servico' => 1)); // tipo_servico  = 1 para cobrança registrada, 2 para sem registro
+        $lote              = $arquivo->addLote(array('tipo_servico' => 1)); // tipo_servico  = 1 para cobrança registrada, 2 para sem registro
 
-        $lote->inserirDetalhe(array(
-            'codigo_ocorrencia' => 1, //1 = Entrada de título, para outras opçoes ver nota explicativa C004 manual Cnab_SIGCB na pasta docs
-            'nosso_numero' => $dados->nosso_numero, // numero sequencial de boleto
-            'seu_numero' => 1, // se nao informado usarei o nosso numero 
-
-            /* campos necessarios somente para itau cnab400, não precisa comentar se for outro layout    */
-            'carteira_banco' => $dados->carteira, // codigo da carteira ex: 109,RG esse vai o nome da carteira no banco
-            'cod_carteira' => "I", // I para a maioria ddas carteiras do itau
-            /* campos necessarios somente para itau, não precisa comentar se for outro layout   */
-            'especie_titulo' => "DM", // informar dm e sera convertido para codigo em qualquer laytou conferir em especie.php
-            'valor' => $dados->valor, // Valor do boleto como float valido em php
-            'emissao_boleto' => 2, // tipo de emissao do boleto informar 2 para emissao pelo beneficiario e 1 para emissao pelo banco
-            'protestar' => 2, // 1 = Protestar com (Prazo) dias, 2 = Devolver após (Prazo) dias
-            'nome_pagador' => "JOSÉ da SILVA ALVES", // O Pagador é o cliente, preste atenção nos campos abaixo
-            'tipo_inscricao' => 1, //campo fixo, escreva '1' se for pessoa fisica, 2 se for pessoa juridica
-            'numero_inscricao' => '123.122.123-56', //cpf ou ncpj do pagador
-            'endereco_pagador' => 'Rua dos developers,123 sl 103',
-            'bairro_pagador' => 'Bairro da insonia',
-            'cep_pagador' => '12345-123', // com hífem
-            'cidade_pagador' => 'Londrina',
-            'uf_pagador' => 'PR',
-            'data_vencimento' => '2016-04-09', // informar a data neste formato
-            'data_emissao' => '2016-04-09', // informar a data neste formato
-            'vlr_juros' => 0.15, // Valor do juros de 1 dia'
-            'data_desconto' => '2016-04-09', // informar a data neste formato
-            'vlr_desconto' => '0', // Valor do desconto
-            'prazo' => 5, // prazo de dias para o cliente pagar após o vencimento
-            'mensagem' => 'JUROS de R$0,15 ao dia'.PHP_EOL."Não receber apos 30 dias",
-            'email_pagador' => 'rogerio@ciatec.net', // data da multa
-            'data_multa' => '2016-04-09', // informar a data neste formato, // data da multa
-            'valor_multa' => 30.00, // valor da multa
-        ));
-
-        header("Content-type: text/plain");
-        header("Content-Disposition: attachment; filename=remessa.txt");
+        foreach ($dados->detalhes as $boleto) {
+            $lote->inserirDetalhe(array(
+                'codigo_ocorrencia' => 1, //1 = Entrada de título, para outras opçoes ver nota explicativa C004 manual Cnab_SIGCB na pasta docs
+                'nosso_numero' => $boleto->nosso_numero, // numero sequencial de boleto
+                'seu_numero' => 1, // se nao informado usarei o nosso numero 
+                /* campos necessarios somente para itau cnab400, não precisa comentar se for outro layout    */
+                'carteira_banco' => $boleto->carteira, // codigo da carteira ex: 109,RG esse vai o nome da carteira no banco
+                'cod_carteira' => "I", // I para a maioria ddas carteiras do itau
+                /* campos necessarios somente para itau, não precisa comentar se for outro layout   */
+                'especie_titulo' => "DM", // informar dm e sera convertido para codigo em qualquer laytou conferir em especie.php
+                'valor' => $boleto->valor, // Valor do boleto como float valido em php
+                'emissao_boleto' => 2, // tipo de emissao do boleto informar 2 para emissao pelo beneficiario e 1 para emissao pelo banco
+                'protestar' => 2, // 1 = Protestar com (Prazo) dias, 2 = Devolver após (Prazo) dias
+                'nome_pagador' => "JOSÉ da SILVA ALVES", // O Pagador é o cliente, preste atenção nos campos abaixo
+                'tipo_inscricao' => 1, //campo fixo, escreva '1' se for pessoa fisica, 2 se for pessoa juridica
+                'numero_inscricao' => '123.122.123-56', //cpf ou ncpj do pagador
+                'endereco_pagador' => 'Rua dos developers,123 sl 103',
+                'bairro_pagador' => 'Bairro da insonia',
+                'cep_pagador' => '12345-123', // com hífem
+                'cidade_pagador' => 'Londrina',
+                'uf_pagador' => 'PR',
+                'data_vencimento' => '2016-04-09', // informar a data neste formato
+                'data_emissao' => '2016-04-09', // informar a data neste formato
+                'vlr_juros' => 0.15, // Valor do juros de 1 dia'
+                'data_desconto' => '2016-04-09', // informar a data neste formato
+                'vlr_desconto' => '0', // Valor do desconto
+                'prazo' => 5, // prazo de dias para o cliente pagar após o vencimento
+                'mensagem' => 'JUROS de R$0,15 ao dia'.PHP_EOL."Não receber apos 30 dias",
+                'email_pagador' => 'rogerio@ciatec.net', // data da multa
+                'data_multa' => '2016-04-09', // informar a data neste formato, // data da multa
+                'valor_multa' => 30.00, // valor da multa
+            ));
+        }
+//        header("Content-type: text/plain");
+//        header("Content-Disposition: attachment; filename=remessa.txt");
         print $arquivo->getText();
     }
 
     function geraCaixa($dados)
     {
-        $arquivo = new Remessa(104, 'cnab240_SIGCB',
+        $registros         = $this->mongodb->remessas->count();
+        $numero_sequencial = $registros + 1;
+        $arquivo           = new Remessa(104, 'cnab240_SIGCB',
             array(
             'nome_empresa' => "Empresa ABC", // seu nome de empresa
             'tipo_inscricao' => 2, // 1 para cpf, 2 cnpj 
@@ -76,7 +87,7 @@ final class RemessaController
             'codigo_beneficiario' => '123456', // codigo fornecido pelo banco
             'numero_sequencial_arquivo' => $dados->nosso_numero, // sequencial do arquivo um numero novo para cada arquivo gerado
         ));
-        $lote    = $arquivo->addLote(array('tipo_servico' => 1)); // tipo_servico  = 1 para cobrança registrada, 2 para sem registro
+        $lote              = $arquivo->addLote(array('tipo_servico' => 1)); // tipo_servico  = 1 para cobrança registrada, 2 para sem registro
 
         $lote->inserirDetalhe(array(
             'codigo_ocorrencia' => 1, //1 = Entrada de título, para outras opçoes ver nota explicativa C004 manual Cnab_SIGCB na pasta docs
